057f8d7880bc17a4b0665814d1b1a01b5976decc
weiyi <ma.weiyi@dhs.sg>
2021-07-02 18:35:53 +0800

    add manageword

---
+++ Manageword/manage.py
@@ -0,0 +1,53 @@
+import os.path
+
+def newPass():
+    file = open("info.txt", 'a')
+
+    print("\n\n")
+
+    username = input("Please enter the username: ")
+    password = input("Please enter the password here: ")
+    website = input("Please enter the website address here: ")
+
+    print("\n\n")
+
+    writeUsername = "Username: " + username + "\n"
+    writePassword = "Password: " + password + "\n"
+    writeWebsite = "Website: " + website + "\n"
+
+    file.write("-----------------------\n")
+    file.write(writeUsername)
+    file.write(writePassword)
+    file.write(writeWebsite)
+    file.write("-----------------------\n\n")
+    file.close()
+
+def readPass():
+    file = open('info.txt', 'r')
+    content = file.read()
+    file.close()
+    print(content)
+
+def checkFile():
+    if os.path.exists("info.txt"):
+        pass
+
+    else:
+        file = open("info.txt", 'w')
+        file.close()
+
+print("Welcome to Manageword, the password manager right in your terminal.\n")
+print("1. Print passwords\n2. Add new password")
+choice = input("Input your choice (enter the number): ")
+
+if choice == "1":
+    readPass()
+elif choice == "2":
+    newPass()
+else:
+    print("Invalid input!")
+
+
+
+
+
