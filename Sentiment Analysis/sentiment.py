0b162db6aa48ab9bccab55628ad41b3505d13e7b
weiyi <ma.weiyi@dhs.sg>
2021-07-02 22:19:50 +0800

    add sentiment analysis

---
+++ Sentiment Analysis/sentiment.py
@@ -0,0 +1,34 @@
+import argparse
+from google.cloud import language_v1
+
+def print_result(annotations):
+    score = annotations.document_sentiment.score
+    magnitude = annotations.document_sentiment.magnitude
+
+    for index, sentence in enumerate(annotations.sentences):
+        sentence_sentiment = sentence.sentiment.score
+        print("Sentence {} has a sentiment score of {}".format(index, sentence_sentiment))
+
+    print("Overall Sentiment: score of {} with magnitude of {}".format(score, magnitude))
+
+    return 0
+
+def analyze(movie_review_filename):
+    client = language_v1.LanguageServiceClient()
+
+    with open(movie_review_filename, "r") as review_file:
+        content = review_file.read()
+
+    document = language_v1.Document(content=content, type_=language_v1.Document.Type.PLAIN_TEXT)
+    annotations = client.analyze_sentiment(request={'document': document})
+
+    print_result(annotations)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    parser.addargument("movie_review_filename", help="The filename of the movie review you'd like to analyze.")
+
+    args = parser.parse_args()
+
+    analyze(args.movie_review_filename)
